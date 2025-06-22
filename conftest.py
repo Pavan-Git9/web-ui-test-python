439b6fdb2be8a8f8ce0122986b4ae5b30bf6feeb
Pavan-Git9 <pavankumar4856@gmail.com>
2025-06-22 23:18:52 +0200

    fix: crossbrowser compatibility check added for chrome and edge

--- conftest.py
+++ conftest.py
@@ -7,44 +7,26 @@ from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.edge.service import Service as EdgeService
-import os
-import tempfile
-import uuid
 
 def pytest_addoption(parser):
     parser.addoption(
         "--browser",
         action="store",
         default="chrome",
-        help="Browser to run tests (chrome, edge)"
+        help="Browser to run tests (chrome, firefox, edge)"
     )
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="module")
 def setup(request):
     browser = request.config.getoption("--browser").lower()
 
     if browser == "chrome":
-        options = webdriver.ChromeOptions()
-        # user_data_dir = os.path.join(tempfile.gettempdir(), f"chrome-profile-{uuid.uuid4()}")
-        # options.add_argument(f"--user-data-dir={user_data_dir}")
-        options.add_argument("--no-first-run")
-        options.add_argument("--no-default-browser-check")
-        options.add_argument("--disable-popup-blocking")
-        options.add_argument("--disable-extensions")
-        options.add_argument("--disable-infobars")
-        options.add_argument("--start-maximized")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--no-sandbox")
-        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-
+        service = ChromeService(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service)
 
     elif browser == "edge":
-        options = webdriver.EdgeOptions()
-        user_data_dir = os.path.join(tempfile.gettempdir(), f"edge-profile-{uuid.uuid4()}")
-        options.add_argument(f"--user-data-dir={user_data_dir}")
-        options.add_argument("--no-first-run")
-        options.add_argument("--start-maximized")
-        driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()), options=options)
+        service = EdgeService(EdgeChromiumDriverManager().install())
+        driver = webdriver.Edge(service=service)
 
     else:
         raise ValueError(f"Unsupported browser: {browser}")
