eb45d0d22d7fb15dea495012fde5a3f60978bcfd
Pavan-Git9 <pavankumar4856@gmail.com>
2025-06-22 23:03:55 +0200

    fix: workflow updated with selenium package installation

--- conftest.py
+++ conftest.py
@@ -7,13 +7,16 @@ from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.edge.service import Service as EdgeService
+import os
+import tempfile
+import uuid
 
 def pytest_addoption(parser):
     parser.addoption(
         "--browser",
         action="store",
         default="chrome",
-        help="Browser to run tests (chrome, firefox, edge)"
+        help="Browser to run tests (chrome, edge)"
     )
 
 @pytest.fixture(scope="module")
@@ -21,12 +24,23 @@ def setup(request):
     browser = request.config.getoption("--browser").lower()
 
     if browser == "chrome":
-        service = ChromeService(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=service)
+        options = webdriver.ChromeOptions()
+        options.add_argument("--no-first-run")
+        options.add_argument("--no-default-browser-check")
+        options.add_argument("--disable-popup-blocking")
+        options.add_argument("--disable-extensions")
+        options.add_argument("--disable-infobars")
+        options.add_argument("--start-maximized")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--no-sandbox")
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+
 
     elif browser == "edge":
-        service = EdgeService(EdgeChromiumDriverManager().install())
-        driver = webdriver.Edge(service=service)
+        options = webdriver.EdgeOptions()
+        user_data_dir = os.path.join(tempfile.gettempdir(), f"edge-profile-{uuid.uuid4()}")
+        options.add_argument(f"--user-data-dir={user_data_dir}")
+        driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()), options=options)
 
     else:
         raise ValueError(f"Unsupported browser: {browser}")
@@ -35,6 +49,3 @@ def setup(request):
     driver.maximize_window()
     yield driver
     driver.quit()
-#    driver.maximize_window()
-#    yield driver
-#    driver.quit()
